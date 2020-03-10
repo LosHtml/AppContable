3edd7cda8dad502b217c0857fbe8c60aeb6490cf
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2020-03-09 19:34:12 -0500

    ARREGLANDO
    a

--- config/Conexion.php
+++ config/Conexion.php
@@ -8,7 +8,9 @@ if ($mysqli -> connect_errno) {
     die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno() 
         . ") " . $mysqli -> mysqli_connect_error());
 }
-else
+else {
+    
+}
 
 
 ?>
