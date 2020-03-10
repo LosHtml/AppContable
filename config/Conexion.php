7f1c6d23d067c44ad692aaa0b06a54ed122c3278
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2020-03-09 19:31:56 -0500

    ARREGLANDO ANDO conexion.php

--- config/Conexion.php
+++ config/Conexion.php
@@ -3,12 +3,12 @@
 
 
 
-		$mysqli = new MySQLi("localhost", "root","", "dbprincipal");
-		if ($mysqli -> connect_errno) {
-			die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno() 
-				. ") " . $mysqli -> mysqli_connect_error());
-		}
-		else
-		
+$mysqli = new MySQLi("localhost", "root","", "dbprincipal");
+if ($mysqli -> connect_errno) {
+    die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno() 
+        . ") " . $mysqli -> mysqli_connect_error());
+}
+else
+
 
 ?>
